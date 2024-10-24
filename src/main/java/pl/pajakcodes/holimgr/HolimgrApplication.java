bb924e9b7e237d4b1c41a5e270c95a382f9d92fe
PeterJavaDev <piotr.pajak@pajakcodes.pl>
2024-10-24 18:32:43 +0200

    App structure

---
+++ src/main/java/pl/pajakcodes/holimgr/HolimgrApplication.java
@@ -0,0 +1,13 @@
+package pl.pajakcodes.holimgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolimgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolimgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pajakcodes/holimgr/HolimgrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pajakcodes.holimgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolimgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
